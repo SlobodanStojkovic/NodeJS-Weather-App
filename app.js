389c362901650e433b8ea51d4b91aa8048bed205
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-05-12 00:44:41 +0200

    Add User can now search for data dynamicly in terminal

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Belgrade", (error, data) => {
-  if (error) {
-    return console.log("Error", error);
-  }
-  console.log("Location details:", data);
+const address = process.argv[2];
 
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+  console.log("Please provide address");
+} else {
+  geocode(address, (error, data) => {
     if (error) {
-      return console.log(error);
+      return console.log("Error", error);
     }
+    console.log("Location details:", data);
 
-    console.log(data.location);
-    console.log(forecastData);
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Weather details:", forecastData);
+    });
   });
-});
+}
